05cf14cd88f296f0aaba6cf6a4e3e3df06a99fb0
Suzzie <129015477+Suzzie234@users.noreply.github.com>
2024-12-17 19:12:49 +0000

    login.php

--- login.php
+++ login.php
@@ -1,3 +1,4 @@
+
 <!DOCTYPE html>
 <html lang="en">
     <head>
@@ -7,7 +8,7 @@
         
         <title>Escape Room</title>
 
-        <link rel="stylesheet" href="css/style.css">
+        <link rel="stylesheet" href="assets/css/style.css">
         <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
         <link href="https://fonts.cdnfonts.com/css/arcade-classic" rel="stylesheet">
 
@@ -19,25 +20,25 @@
 
     </head>
     
-    <body>
-        <div class="wrapper">
-            <form action="">
-                <h1>Login</h1>
+    <body> 
+
+        <div class="wrapper">            
+            <form action="http://localhost/Emoji%20Escape%20Room/actions/login_process.php" method="post">
+                <h1>Login<h1>
                 <div class="input-box">
-                    <input type="text" placeholder="Username" required>
+                    <input type="username" placeholder="Username" name="username" id="username" required>
                     <i class='bx bxs-user'></i>
                 </div>
                 <div class="input-box">
-                    <input type="password" placeholder="Password" required>
+                    <input type="password" placeholder="Password" name="password" id="password" required>
                     <i class='bx bxs-lock-alt'></i>
                 </div>
                 <div class="remember-forgot">
-                    <label><input type="checkbox">Remember Me</label>
                     <a href="#">Forgot Password?</a>
                 </div>
                 <button type="submit" class="btn">Login</button>
                 <div class="register-link">
-                    <p>Don't have an account?<a href="signup.html">Register</a></p>
+                    <p>Don't have an account?<a href="signup.php">Register</a></p>
                 </div>
             </form>
         </div>
