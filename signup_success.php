9d643ffbc6219576de2561f1d935d1cd8dfc79fa
Suzzie <129015477+Suzzie234@users.noreply.github.com>
2024-12-17 22:31:21 +0000

    Update and rename game.php to signup_success.php

---
+++ signup_success.php
@@ -0,0 +1,28 @@
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        
+        <title>Escape Room</title>
+
+        <link rel="stylesheet" href="assests/css/signup_success.css">
+        <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+        <link href="https://fonts.cdnfonts.com/css/arcade-classic" rel="stylesheet">
+
+        <link rel="stylesheet" href="fonts/material-icon/css/material-design-iconic-font.min.css">
+
+        <!-- Adding a favicon-->
+         <link rel ="icon" type= "image/png" href="https://cdn-icons-png.flaticon.com/128/3483/3483089.png">
+</head>
+<body>
+    
+    <h1>We're almost there..</h1>
+    
+    <p>Signup successful.
+       You can now <a href="http://localhost/Emoji%20Escape%20Room/login.php">Log in</a>.</p>
+    
+</body>
+</html>
